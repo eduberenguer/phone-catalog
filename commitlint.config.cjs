25128366ca09fff584aab63581cffbe2202a66d9
Edu Berenguer Aragon <eddiberenguer@gmail.com>
2026-09-21 15:21:43 +0200

    style: apply Prettier formatting now that config is set

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,9 +1,9 @@
 // Conventional Commits: <type>(<scope>): <summary>. Types come from
 // @commitlint/config-conventional (feat, fix, chore, docs, refactor...).
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    'header-max-length': [2, 'always', 72],
-    'subject-case': [0], // allow capitalized proper nouns (Figma, API...)
+    "header-max-length": [2, "always", 72],
+    "subject-case": [0], // allow capitalized proper nouns (Figma, API...)
   },
 };
