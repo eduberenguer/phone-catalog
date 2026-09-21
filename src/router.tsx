ee6b278b2a95d02208129771b2a10696bef4af21
Edu Berenguer Aragon <eddiberenguer@gmail.com>
2026-09-21 20:16:07 +0200

    feat(router): wire layout into nested routes

--- src/router.tsx
+++ src/router.tsx
@@ -3,15 +3,18 @@ import { PhoneList } from "./pages/PhoneList/PhoneList";
 import { PhoneDetail } from "./pages/PhoneDetail/PhoneDetail";
 import { Cart } from "./pages/Cart/Cart";
 import { NotFound } from "./pages/NotFound/NotFound";
+import { Layout } from "./components/Layout/Layout";
 
 export function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<PhoneList />} />
-        <Route path="/phone/:id" element={<PhoneDetail />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="*" element={<NotFound />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<PhoneList />} />
+          <Route path="/phone/:id" element={<PhoneDetail />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
