d498d69357905d735f2ec3d9892e6c2ac6d9b63c
Edu Berenguer Aragon <eddiberenguer@gmail.com>
2026-09-23 11:48:05 +0200

    feat(scroll): reset scroll position on route change

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../Navbar/Navbar";
+import { ScrollToTop } from "../ScrollToTop/ScrollToTop";
 
 import container from "../../styles/container.module.css";
 import styles from "./Layout.module.css";
@@ -7,6 +8,7 @@ import styles from "./Layout.module.css";
 export function Layout() {
   return (
     <div className={styles.app}>
+      <ScrollToTop />
       <Navbar />
       <main className={`${container.container} ${styles.main}`}>
         <Outlet />
--- src/test/setup.ts
+++ src/test/setup.ts
@@ -2,6 +2,8 @@ import "@testing-library/jest-dom/vitest";
 import { afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
 
+window.scrollTo = () => {};
+
 afterEach(() => {
   cleanup();
 });
