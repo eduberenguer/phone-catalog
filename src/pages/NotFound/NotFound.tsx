d33118bfc7c46c4823a45cfb83943d1c585ca086
Edu Berenguer Aragon <eddiberenguer@gmail.com>
2026-09-23 11:48:18 +0200

    feat: set a dynamic document title per page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
+import { usePageTitle } from "../../hooks/usePageTitle";
 
 import styles from "./NotFound.module.css";
 
 export function NotFound() {
+  usePageTitle("Page not found");
+
   return (
     <div className={styles.page}>
       <h1 className={styles.code}>404</h1>
